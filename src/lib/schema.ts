56770b89f05b6cb7a2aed26e383193c1c6a7c1fa
Prince Eze <pchidireeze@gmail.com>
2025-02-13 16:07:37 +0100

    feat: update form schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -7,6 +7,8 @@ export const formSchema = z.object({
   quantity: z.string().min(1).max(5),
   name: z.string().min(2, "Name must be at least 2 characters"),
   email: z.string().email("Invalid email address"),
+  specialRequest: z.string().optional(),
+  profilePhotoUrl: z.string().optional(),
 });
 
 export type FormData = z.infer<typeof formSchema>;
